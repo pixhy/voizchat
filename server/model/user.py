a10c93d44ffa9330ac31e83fd27a8a85953a41a6
pixhy <baktunde95@gmail.com>
2024-12-30 00:34:57 +0100

    user model for sqlite created

---
+++ server/model/user.py
@@ -0,0 +1,19 @@
+﻿
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from pydantic import BaseModel
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: str = Field(unique=True)
+    username: str = Field(index=True)
+    passwordhash: str
+
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    password: str
+
+class UserInfo(BaseModel):
+    id: int
+    email: str
+    username: str
