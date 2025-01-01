25e223471306d872ed9c64dd6f8ccc2bcc311586
pixhy <baktunde95@gmail.com>
2025-01-01 23:24:35 +0100

    new routes

--- webclient/src/router/index.ts
+++ webclient/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../components/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,15 +12,27 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
     {
       path: '/login',
       name: 'login',
-      component: Login,
+      component: () => import('../components/Login.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../components/Register.vue'),
+    },
+    {
+      path: '/account-recovery',
+      name: 'account-recovery',
+      component: () => import('../components/ForgottenPassword.vue'),
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: () => import('../components/authorized/Messages.vue'),
     },
   ],
 })
