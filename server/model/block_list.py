9f034881b79f1bf9f92ea2b5bd08df863d3936db
pixhy <baktunde95@gmail.com>
2025-01-12 16:47:41 +0100

    created model for friend list and block list

---
+++ server/model/block_list.py
@@ -0,0 +1,9 @@
+﻿from sqlmodel import Field, SQLModel
+
+
+class BlockListEntry(SQLModel, table=True):
+    __tablename__ : str = "block_list"
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    blocked_id: int = Field(foreign_key="user.id")
+    date_blocked: int
---
+++ server/model/friend_list.py
@@ -0,0 +1,10 @@
+﻿from sqlmodel import Field, SQLModel
+
+
+class FriendListEntry(SQLModel, table=True):
+    __tablename__ : str = "friend_list"
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    friend_id: int = Field(foreign_key="user.id")
+    pending: bool = True
+    date_added: int | None
